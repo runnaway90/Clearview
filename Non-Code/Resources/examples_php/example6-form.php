c8db52c314316702e055a53ea5782063f7429ec4
Georgi Ivanov <georgi.ivanov91@gmail.com>
2012-02-23 15:25:31 +0000

    The PHP presentation from today with examples

---
+++ Non-Code/Resources/examples_php/example6-form.php
@@ -0,0 +1,5 @@
+<form action="example6-php.php" method="post">
+Name: <input type="text" name="fname" />
+Age: <input type="text" name="age" />
+<input type="submit" />
+</form>
---
+++ Non-Code/Resources/examples_php/example6-php.php
@@ -0,0 +1,2 @@
+Welcome <?php echo $_POST["fname"]; ?><br />
+You are <?php echo $_POST["age"]; ?> years old!
