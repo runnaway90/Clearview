feea7c1116c5e30cd41c377c97eb9386b9ead1f0
Georgi Ivanov <georgi.ivanov91@gmail.com>
2012-03-07 17:02:18 +0000

    modified:   march/index.php 	modified:   march/login/db_config.php

--- march/index.php
+++ march/index.php
@@ -1,1 +1,1 @@
-<?phpif (true) {     header ('Location: intro.php');}else {    header ('Location: main.php');}?>
+<?phprequire_once('login/login_functions.php');if (isLoggedIn()) {     header ('Location: intro.php');}else {    header ('Location: main.php');}?>
