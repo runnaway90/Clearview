98e03330610f2d4f7df676f40cf61eff6e624b47
Georgi Ivanov <georgi.ivanov91@gmail.com>
2012-03-07 16:30:22 +0000

    new file:   march/login/clearvie_database.sql 	new file:   march/login/db_config.php 	new file:   march/login/fb.php 	new file:   march/login/fb_login.png 	new file:   march/login/login.php 	new file:   march/login/logout.php 	new file:   march/login/php_errorlog 	new file:   march/login/register.php

---
+++ march/login/login_functions.php
@@ -0,0 +1,29 @@
+<?
+function validateUser()
+{
+    session_regenerate_id (); //this is a security measure
+    $_SESSION['valid'] = 1;
+    //$_SESSION['username'] = $username;
+}     
+
+function isLoggedIn()
+{
+    if(isset($_SESSION['valid']) && $_SESSION['valid'])
+        return true;
+    return false;
+}
+
+function logout()
+{
+    $_SESSION = array(); //destroy all of the session variables
+    session_destroy();
+}
+
+//make sure that the data is protected from code injection
+function secure_data($data){
+$data=trim($data);
+$data=htmlspecialchars($data);
+$data=mysql_real_escape_string($data);
+return $data;
+}
+?>
